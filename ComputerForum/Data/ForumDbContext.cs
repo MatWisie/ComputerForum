d21b5b1494097c241861579795c3c917f7a71986
MatWisie <Mateuszwisiecki@gmail.com>
2023-03-12 19:36:10 +0100

    Solved cascade delete error, added initial migration.

--- ComputerForum/Data/ForumDbContext.cs
+++ ComputerForum/Data/ForumDbContext.cs
@@ -10,6 +10,29 @@ namespace ComputerForum.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+               .HasMany(e => e.Topics)
+               .WithOne(e => e.User)
+               .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<User>()
+               .HasMany(e => e.ClickedReputations)
+               .WithOne(e => e.User)
+               .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<User>()
+               .HasMany(e => e.Comments)
+               .WithOne(e => e.User)
+               .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<User>()
+               .HasMany(e => e.ReportCreators)
+               .WithOne(e => e.ReportCreator)
+               .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<User>()
+               .HasMany(e => e.ReportedUsers)
+               .WithOne(e => e.ReportedUser)
+               .OnDelete(DeleteBehavior.Restrict);
+        }
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<ClickedReputation> ClickedReputations { get; set; }
