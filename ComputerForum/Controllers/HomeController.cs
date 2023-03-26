3cc0db4bdb1763029534eac92ac2a3e1b399b472
Wiktor <109503651+wiktor-wojciechowski@users.noreply.github.com>
2023-03-26 19:17:12 +0200

    Added LogIn View, added link to LogIn in Register View

--- ComputerForum/Controllers/HomeController.cs
+++ ComputerForum/Controllers/HomeController.cs
@@ -26,6 +26,10 @@ namespace ComputerForum.Controllers
         {
             return View();
         }
+        public IActionResult LogIn()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
